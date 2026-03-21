1d40e53f9f2c11257615fe232386c9152d658683
Saidjon Qurbonov <s.b.qurbonov@wlv.ac.uk>
2026-03-21 20:20:30 +0500

    Update AppServiceProvider.php

--- backend_codes/sus_version_0_0_3_1/app/Providers/AppServiceProvider.php
+++ backend_codes/sus_version_0_0_3_1/app/Providers/AppServiceProvider.php
@@ -4,25 +4,23 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Http\Response;
+use Illuminate\Support\Facades\URL; // <--- MUST ADD THIS
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (config('app.env') === 'production') {
+        // Change this to check if it's NOT local, 
+        // or just force it if you are sure your DO droplet uses SSL
+        if (config('app.env') === 'production' || config('app.env') === 'staging') {
             URL::forceScheme('https');
         }
+
         if (app()->environment('local')) {
             Response::macro('apiJson', function ($data = [], $status = 200) {
                 return response()->json($data, $status)
