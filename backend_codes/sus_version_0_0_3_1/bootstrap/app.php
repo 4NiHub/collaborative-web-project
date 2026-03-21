c71885b07faa5ab7738986d22058cd8325e4ade1
Saidjon Qurbonov <s.b.qurbonov@wlv.ac.uk>
2026-03-21 20:55:05 +0500

    Update app.php

--- backend_codes/sus_version_0_0_3_1/bootstrap/app.php
+++ backend_codes/sus_version_0_0_3_1/bootstrap/app.php
@@ -4,8 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-// 1. Initialize the application builder
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
@@ -27,13 +26,8 @@ $app = Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions) {
         //
     })
+    ->booting(function ($app) {
+        // This runs at the correct time to fix the storage path
+        $app->useStoragePath($app->basePath() . '/storage');
+    })
     ->create();
-
-// 2. Fix the "Invalid Cache Path" error by forcing the storage path
-// This ensures Laravel knows exactly where its folders are on DigitalOcean
-$app->useStoragePath($app->basePath() . '/storage');
-
-// 3. Manually set the view compiled path to avoid the missing config/view.php issue
-config(['view.compiled' => realpath(storage_path('framework/views'))]);
-
-return $app;
